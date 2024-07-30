58ba0b2cfc3d31db46d009be9c3678b42262b9af
dusknobo <150761350+dusknobo@users.noreply.github.com>
2024-07-30 18:25:05 +0800

    add neogit

--- lua/community.lua
+++ lua/community.lua
@@ -18,4 +18,6 @@ return {
   { import = "astrocommunity.split-and-window.windows-nvim" },
 
   { import = "astrocommunity.colorscheme.nordic-nvim" },
+
+  { import = "astrocommunity.git.neogit" },
 }
