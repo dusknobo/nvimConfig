3f18d068822c5abd5e5ebc1fecf41a74132c64fd
dusknobo <150761350+dusknobo@users.noreply.github.com>
2025-07-16 21:34:07 +0800

    Update community.lua

--- lua/community.lua
+++ lua/community.lua
@@ -9,4 +9,16 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
+    { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.cmake" },
+  { import = "astrocommunity.pack.python" },
+
+  { import = "astrocommunity.split-and-window.mini-map" },
+  { import = "astrocommunity.split-and-window.windows-nvim" },
+
+  { import = "astrocommunity.colorscheme.nordic-nvim" },
+
+  { import = "astrocommunity.git.neogit" },
 }
